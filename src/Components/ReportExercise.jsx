718f0a9324106edae7b16dcb57f807b4319887c4
vincibusa <vincibusa@gmail.com>
2024-12-12 13:03:21 +0100

    build

--- src/Components/ReportExercise.jsx
+++ src/Components/ReportExercise.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { FaHome, FaRedoAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
