d6ace2e6f152604255822cbd2166f72499863f09
giovanni <workgiovannic@gmail.com>
2024-12-19 22:02:11 +0100

    Revert "fix error"
    This reverts commit 77dad169752558a3aff4ada6fa7b094128a8a8d4.

--- src/Pages/Squat/hooks/useSquatValidation.js
+++ src/Pages/Squat/hooks/useSquatValidation.js
@@ -98,20 +98,21 @@ const useSquatValidation = ({ onValidRep, onInvalidRep, onTotalRep }) => {
           if (newSequence.length === 5) {
             onTotalRep();
             
+            // Check if sequence is valid
             const isSequenceValid = validateStageSequence(newSequence);
-            const hasFormFaults = formFaultsRef.current.length > 0;
             
-            if (isSequenceValid && !hasFormFaults) {
+            // Even if sequence is valid, rep is invalid if there are form faults
+            if (isSequenceValid && formFaultsRef.current.length === 0) {
               onValidRep();
               showToastIfAllowed('Squat valido!', 'success');
             } else {
-              onInvalidRep();
-              const message = hasFormFaults
+            
+              const message = formFaultsRef.current.length > 0
                 ? `Squat non valido!\nMotivo:\n${formFaultsRef.current.join('\n')}`
                 : 'Squat non valido!';
+                onInvalidRep();
               showToastIfAllowed(message, 'error');
             }
-            
             formFaultsRef.current = [];
             return [];
           }
